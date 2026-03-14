0839108cb1dfd0682ac85ba57f03af97fc29e239
IttoSanzzo <konohittos@gmail.com>
2026-03-14 15:12:21 -0300

    Stable pre-release

--- src/main/java/io/github/ittosanzzo/albinaprops/common/registry/ModItems.java
+++ src/main/java/io/github/ittosanzzo/albinaprops/common/registry/ModItems.java
@@ -24,10 +24,14 @@ public class ModItems {
 		ITEMS.register(context.getModEventBus());
 	}
 
-	public static final RegistryObject<Item> KUREHIME = registerPropPng("kurehime");
-	public static final RegistryObject<Item> HOROLOGIUN_LAUFORD = registerPropPng("horologiun_lauford");
-	public static final RegistryObject<Item> VIOLIETCH = registerPropPng("violietch");
 	public static final RegistryObject<Item> CARMEN_AB_INITIO = registerPropPng("carmen_ab_initio");
+	// public static final RegistryObject<Item> KUREHIME = registerPropPng("kurehime");
+	public static final RegistryObject<Item> HOROLOGIUN_LAUFORD = registerPropPng("horologiun_lauford");
+	public static final RegistryObject<Item> CHAINED_HOROLOGIUN_LAUFORD = registerPropPng("chained_horologiun_lauford");
+	public static final RegistryObject<Item> GOLDEN_HOROLOGIUN_LAUFORD = registerPropPng("golden_horologiun_lauford");
+	public static final RegistryObject<Item> CHAINER_GOLDEN_HOROLOGIUN_LAUFORD = registerPropPng("chained_golden_horologiun_lauford");
+	public static final RegistryObject<Item> VIOLITCH = registerPropPng("violitch");
+	public static final RegistryObject<Item> OPEN_VIOLITCH = registerPropPng("open_violitch");
 	public static final RegistryObject<Item> TEA = registerPropPng("tea");
 	public static final RegistryObject<Item> TEA_POT = registerPropPng("tea_pot");
 }
